d2460a9956dcc7f3bcd04f4c9707d9e06b5259b5
Frank Bültge <frank@bueltge.de>
2012-04-26 12:19:08 +0200

    exclude the script for easy to use on custom sites

--- media-query-debugger.php
+++ media-query-debugger.php
@@ -93,35 +93,7 @@
 		</p> 
 	</footer>
 	
-	<script>
-	(function() {
-		var rx = /(&|\?)(\w+)=true/gi, str = window.location.href, m, s = '', gup = {};
-		while (m = rx.exec(str)) {
-			s += ' ' + m[2];
-			gup[m[2]] = true;
-		}
-		
-		if ( typeof(window.innerWidth) != 'undefined') {
-			var head  = document.head,
-				style = document.createElement('style'),
-				rules = document.createTextNode('body[data-mqd]:before {content:attr(data-mqd);}');
-			
-			style.type = 'text/css';
-			
-			if (style.styleSheet)
-				style.styleSheet.cssText = rules.nodeValue;
-			else
-				style.appendChild(rules);
-			
-			head.appendChild(style);
-			
-			document.body.setAttribute('data-mqd', window.innerWidth + ' x ' + window.innerHeight);
-			window.onresize = function(event) {
-				document.body.setAttribute('data-mqd', window.innerWidth + ' x ' + window.innerHeight);
-			};
-		}
-	})();
-	</script>
+	<script src="script.js"></script>
 	<script src="http://bueltge.de/mv/?js"></script>
 	
 </body>
